207b47dd6a7a35a3a22b2cb83edbd58dd4aadb68
Sharayu Divekar <sharayudivekar9@gmail.com>
2018-08-05 18:47:07 +0530

    First check-in

---
+++ src/in/co/shara/example/files/FileHandler.java
@@ -0,0 +1,22 @@
+package in.co.shara.example.files;
+import java.io.*;
+
+public class FileHandler {
+	public static void main(String args[])throws Exception {
+		if(args.length != 1) {
+			System.out.println("Usage: FileHanlder <file-nam>");
+			return;
+		}
+		FileHandler handler = new FileHandler();
+		handler.readFile(args[0]);
+	}
+	
+	public void readFile(String fileName) throws IOException {
+		  File file = new File(fileName);
+		  BufferedReader br = new BufferedReader(new FileReader(file));
+		  String st;
+		  while ((st = br.readLine()) != null)
+		    System.out.println(st);
+		  br.close();
+	}
+}
