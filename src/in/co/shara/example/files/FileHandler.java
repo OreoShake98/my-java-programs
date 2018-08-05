53bf725d22c5fe722d94e5b4444ffacde4d5fbe8
Sharayu Divekar <sharayudivekar9@gmail.com>
2018-08-05 19:36:20 +0530

    CSV File reader added

--- src/in/co/shara/example/files/FileHandler.java
+++ src/in/co/shara/example/files/FileHandler.java
@@ -1,5 +1,11 @@
 package in.co.shara.example.files;
 import java.io.*;
+import java.util.List;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 public class FileHandler {
 	public static void main(String args[])throws Exception {
@@ -9,15 +15,35 @@ public class FileHandler {
 			return;
 		}
 		FileHandler handler = new FileHandler();
-		handler.readFile(args[0]);
+		handler.readCSVFile(args[0]);
 	}
-	
+
 	public void readFile(String fileName) throws IOException {
-		  File file = new File(fileName);
-		  BufferedReader br = new BufferedReader(new FileReader(file));
-		  String st;
-		  while ((st = br.readLine()) != null)
-		    System.out.println(st);
-		  br.close();
+		File file = new File(fileName);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null)
+			System.out.println(st);
+		br.close();
+	}
+
+	public void readCSVFile(String fileName) throws IOException {
+		final CSVParser parser =
+				new CSVParserBuilder()
+				.withSeparator(',')
+				.withIgnoreQuotations(true)
+				.build();
+		final CSVReader reader =
+				new CSVReaderBuilder(new FileReader(fileName))
+				.withSkipLines(1)
+				.withCSVParser(parser)
+				.build();
+		List<String[]> myEntries = reader.readAll();	
+		for(String[] myEntry : myEntries) {
+			for(String field : myEntry) {
+				System.out.println("Field value is " + field);
+			}
+		}
+		reader.close();
 	}
 }
